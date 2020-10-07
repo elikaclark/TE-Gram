e9bbbe330ef782b23b72c6f6ea189516d66cd947
Peter Thongpasouk <thongpasoukpeter@gmail.com>
2020-10-07 12:06:53 -0400

    Working cloudinary api call to upload photos

---
+++ vue/src/services/CloudinaryService.js
@@ -0,0 +1,21 @@
+// const CLOUDINARY_ULR = process.env.CLOUDINARY_URL;
+// const CLOUDINARY_UPLOAD_PRESET = process.env.CLOUDINARY_PRESET;
+
+const CLOUDINARY_ULR = 'https://api.cloudinary.com/v1_1/dtotytpog/image/upload';
+const CLOUDINARY_UPLOAD_PRESET = 'lvizruov';
+
+
+export default {
+
+    newImage(formData){
+        formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+        return fetch(CLOUDINARY_ULR, {
+          method: 'POST',
+          header: {
+              'content-type': 'application/x-www-form-urlencoded'
+          },
+          body: formData
+      });
+    },
+}
