98aa8ecad36526f80f6fcb034851ffe1c7a275db
Peter Thongpasouk <thongpasoukpeter@gmail.com>
2020-10-09 11:14:09 -0400

    added favorites button to add photo to users favorite list. And can remove it from their list

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -13,6 +13,14 @@
           />
           <button type="button" class="btn btn-danger">Likes: {{photo.likes}}</button>
           <button type="button" class="btn btn-primary">Comments</button>
+          <button 
+          v-if="!inFavorites(photo.photo_id)" type="button" class="btn btn-info" @click.prevent="addToFavorite(photo.photo_id)">
+            Favorite
+          </button>
+          <button 
+          v-else type="button" class="btn btn-info" @click.prevent="deleteFromFavorite(photo.photo_id)">
+            Unfavorite
+          </button>
           <div>
             "{{photo.description}}"
           </div>
@@ -28,7 +36,70 @@ export default {
   data() {
     return {
       photos: [],
+      userFavPhotos: [],
     };
+  },
+  methods: {
+    checkUserFavorites(){
+
+      fetch("http://localhost:8080/" + this.$store.state.user.id + "/favorites",{
+      method: "GET"
+      }).then(response => response.json())
+      .then(json =>{
+        this.userFavPhotos = json;
+      }).catch(err =>{
+        console.log(err);
+      });
+
+    },
+    inFavorites(photoId){
+        let isFav = false;
+        this.userFavPhotos.forEach(photo =>{
+          if(photo.photo_id == photoId){
+            isFav = true;
+          }
+        });
+        return isFav;
+    },
+    addToFavorite(photo_id){
+  
+      fetch("http://localhost:8080/addFavorite",{
+        method: "POST",
+        headers:{
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          user_id: this.$store.state.user.id,
+          photo_id: photo_id
+        })
+      }).then(response =>{
+        if(response.status === 201){
+          this.checkUserFavorites();
+        }
+      }).catch(err =>{
+        console.log(err);
+      })
+
+    },
+    deleteFromFavorite(photo_id){
+
+      fetch("http://localhost:8080/deleteFavorite",{
+        method: "DELETE",
+        headers:{
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          user_id: this.$store.state.user.id,
+          photo_id: photo_id
+        })
+      }).then(response =>{
+        if(response.status === 204){
+          this.checkUserFavorites();
+        }
+      }).catch(err =>{
+        console.log(err);
+      })
+    }
   },
   mounted: function () {
     fetch("http://localhost:8080/photos", {
@@ -40,6 +111,9 @@ export default {
       .then((jsonBody) => {
         this.photos = jsonBody.reverse();
       });
+      
+      // updates favorite buttons
+      this.checkUserFavorites();
   },
 };
 </script>
